6293d4f29e1468b79ac249280e458ab117d9f042
Alexander <1eksandr999@gmail.com>
2019-07-21 14:09:04 +0300

    Scope & Context

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,81 +1,90 @@
 class Fighter {
+    #name;
+    #damage;
+    #agility;
+    #health;
+    #maxHealth;
+    #wins;
+    #losses;
+
     constructor({name, damage, agility, hp}) {
-        this.name = name;
-        this.damage = damage;
-        this.agility = agility;
-        this.health = hp;
-        this.maxHealth = hp;
-        this.wins = 0;
-        this.losses = 0;
+        this.#name = name;
+        this.#damage = damage;
+        this.#agility = agility;
+        this.#health = hp;
+        this.#maxHealth = hp;
+        this.#wins = 0;
+        this.#losses = 0;
     }
 
     getName() {
-        return this.name;
+        return this.#name;
     }
 
     getDamage() {
-        return this.damage;
+        return this.#damage;
     }
 
     getAgility() {
-        return this.agility;
+        return this.#agility;
     }
 
     getHealth() {
-        return this.health;
+        return this.#health;
     }
 
     attack(fighter) {
         const max = 100;
-        const successRange = max - fighter.agility;
+        const successRange = max - fighter.#agility;
         const impactForce = Math.floor(Math.random() * max);
 
         if (impactForce > successRange) {
-            console.log(`${this.name} attack missed`);
+            console.log(`${this.#name} attack missed`);
         } else {
-            fighter.dealDamage(this.damage);
-            console.log(`${this.name} make ${this.damage} damage to ${fighter.name}`);
+            fighter.dealDamage(this.#damage);
+            console.log(`${this.#name} make ${this.#damage} damage to ${fighter.#name}`);
         }
     }
 
     logCombatHistory() {
-        console.log(`Name: ${this.name}, Wins: ${this.wins}, Losses: ${this.losses}`);
+        console.log(`Name: ${this.#name}, Wins: ${this.#wins}, Losses: ${this.#losses}`);
     }
 
     heal(health) {
-        this.health = this.health + health > this.maxHealth ? this.maxHealth : this.health + health;
-        console.log(`${this.name} was healed! He has ${this.health} hp`);
+        this.#health = this.#health + health > this.#maxHealth ? this.#maxHealth : this.#health + health;
+        console.log(`${this.#name} was healed! He has ${this.#health} hp`);
     }
 
     dealDamage(damage) {
-        this.health = this.health - damage < 0 ? 0 : this.health - damage;
+        this.#health = this.#health - damage < 0 ? 0 : this.#health - damage;
     }
 
     addWin() {
-        this.wins++;
+        this.#wins++;
     }
 
     addLoss() {
-        this.losses++;
+        this.#losses++;
     }
 }
 
 function battle(firstFighter, secondFighter) {
-    if (firstFighter.health === 0 || secondFighter.health === 0){
-        console.log(`${secondFighter.health === 0 ? secondFighter.name : firstFighter.name} is dead and can't fight!`);
+    if (firstFighter.getHealth() === 0 || secondFighter.getHealth() === 0) {
+        console.log(`${secondFighter.getHealth() === 0 ? secondFighter.getName() : firstFighter.getName()} is dead and can't fight!`);
         return
     }
 
-    while (firstFighter.health > 0 && secondFighter.health > 0) {
-        if (firstFighter.health > 0) {
+    while (firstFighter.getHealth() > 0 && secondFighter.getHealth() > 0) {
+        if (firstFighter.getHealth() > 0) {
             firstFighter.attack(secondFighter)
-        } if (secondFighter.health > 0) {
+        }
+        if (secondFighter.getHealth() > 0) {
             secondFighter.attack(firstFighter)
         }
     }
 
-    firstFighter.health > 0 ? firstFighter.addWin() : firstFighter.addLoss();
-    secondFighter.health > 0 ? secondFighter.addWin() : secondFighter.addLoss();
+    firstFighter.getHealth() > 0 ? firstFighter.addWin() : firstFighter.addLoss();
+    secondFighter.getHealth() > 0 ? secondFighter.addWin() : secondFighter.addLoss();
 
     firstFighter.logCombatHistory();
     secondFighter.logCombatHistory();
